7c9c02f26b0597848fbac611e5ed0bd62ad689ef
lizhi <cqtenq9@gmail.com>
2014-09-23 10:13:58 +0800

    first commit

---
+++ src/chainparams.cpp
@@ -0,0 +1,232 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "chainparams.h"
+
+#include "assert.h"
+#include "core.h"
+#include "protocol.h"
+#include "util.h"
+
+#include <boost/assign/list_of.hpp>
+
+using namespace boost::assign;
+
+//
+// Main network
+//
+
+unsigned int pnSeed[] = 
+{
+	0xc49c0ed8, 0x1df9d243, 0xd9b75a40, 0xdc525e46, 0x72c321b1, 0x4eedeeb2, 0x18271787, 0xce725232,
+    0x892aafc0, 0x979f6751, 0x2210f618, 0xfb529ed8, 0x66a74b3e, 0xef5d132e, 0x3b7a116c, 0x2fe45f55,
+    0x1df9d243, 0xe41f7c70, 0x8f4cd262, 0xb5c29d62, 0x80f1f3a2, 0x47dc614d, 0x6f458b4e, 0x908caa56,
+    0x553ef762, 0x5aec0852, 0x629a4f54, 0x6a10e13c, 0x4f41c547, 0x4476fd59, 0xcaedbc5a, 0x9f806dc1,
+    0x1df9d243, 0xe41f7c70, 0x8f4cd262, 0xb5c29d62, 0x80f1f3a2, 0x47dc614d, 0x6f458b4e, 0x908caa56,
+    0x553ef762, 0x5aec0852, 0x629a4f54, 0x6a10e13c, 0x4f41c547, 0x4476fd59, 0xcaedbc5a, 0x9f806dc1
+};
+
+class CMainParams : public CChainParams {
+public:
+    CMainParams() {
+        // The message start string is designed to be unlikely to occur in normal data.
+        // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+        // a large 4-byte int at any alignment.
+        pchMessageStart[0] = 0xc0;
+        pchMessageStart[1] = 0xc0;
+        pchMessageStart[2] = 0xc0;
+        pchMessageStart[3] = 0xc0;
+        //how to do
+        vAlertPubKey = ParseHex("04d4da7a5dae4db797d9b0644d57a5cd50e05a70f36091cd62e2fc41c98ded06340be5a43a35e185690cd9cde5d72da8f6d065b499b06f51dcfba14aad859f443a");
+        nDefaultPort = 9336;
+        nRPCPort = 9337;
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 20);
+        nSubsidyHalvingInterval = 210000;
+
+        // Build the genesis block. Note that the output of the genesis coinbase cannot
+        // be spent as it did not originally exist in the database.
+        //
+        // CBlock(hash=000000000019d6, ver=1, hashPrevBlock=00000000000000, hashMerkleRoot=4a5e1e, nTime=1231006505, nBits=1d00ffff, nNonce=2083236893, vtx=1)
+        //   CTransaction(hash=4a5e1e, ver=1, vin.size=1, vout.size=1, nLockTime=0)
+        //     CTxIn(COutPoint(000000, -1), coinbase 04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73)
+        //     CTxOut(nValue=50.00000000, scriptPubKey=0x5F1DF16B2B704C8A578D0B)
+        //   vMerkleTree: 4a5e1e
+        const char* pszTimestamp = "NY Times 05/Oct/2011 Steve Jobs, Apple's Visionary, Dies at 56";
+        CTransaction txNew;
+        txNew.vin.resize(1);
+        txNew.vout.resize(1);
+        txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+        txNew.vout[0].nValue = 50 * COIN;
+        txNew.vout[0].scriptPubKey = CScript() << ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
+        genesis.vtx.push_back(txNew);
+        genesis.hashPrevBlock = 0;
+        genesis.hashMerkleRoot = genesis.BuildMerkleTree();
+        genesis.nVersion = 1;
+        genesis.nTime    = 1317972665;
+        genesis.nBits    = 0x1e0ffff0;
+        genesis.nNonce   = 2084524493;
+        
+        hashGenesisBlock = genesis.GetHash();
+        //LogPrintf("Debug 1:hashGenesisBlock=%s",hashGenesisBlock.ToString().c_str());
+        //LogPrintf("Debug 1:genesis.hashMerkleRoot=%s",genesis.hashMerkleRoot.ToString().c_str());
+        assert(genesis.hashMerkleRoot == uint256("0x97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"));
+        assert(hashGenesisBlock == uint256("0x12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2"));
+
+        //how to do feathercoin ?
+        vSeeds.push_back(CDNSSeedData("bitcoin.sipa.be", "seed.bitcoin.sipa.be"));
+        vSeeds.push_back(CDNSSeedData("bluematt.me", "dnsseed.bluematt.me"));
+        vSeeds.push_back(CDNSSeedData("dashjr.org", "dnsseed.bitcoin.dashjr.org"));
+        vSeeds.push_back(CDNSSeedData("bitcoinstats.com", "seed.bitcoinstats.com"));
+        vSeeds.push_back(CDNSSeedData("bitnodes.io", "seed.bitnodes.io"));
+        vSeeds.push_back(CDNSSeedData("xf2.org", "bitseed.xf2.org"));
+
+        base58Prefixes[PUBKEY_ADDRESS] = list_of(0);
+        base58Prefixes[SCRIPT_ADDRESS] = list_of(5);
+        base58Prefixes[SECRET_KEY] =     list_of(128);
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E);
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4);
+        
+        // Convert the pnSeeds array into usable address objects.
+        for (unsigned int i = 0; i < ARRAYLEN(pnSeed); i++)
+        {
+            // It'll only connect to one or two seed nodes because once it connects,
+            // it'll get a pile of addresses with newer timestamps.
+            // Seed nodes are given a random 'last seen time' of between one and two
+            // weeks ago.
+            const int64_t nOneWeek = 7*24*60*60;
+            struct in_addr ip;
+            memcpy(&ip, &pnSeed[i], sizeof(ip));
+            CAddress addr(CService(ip, GetDefaultPort()));
+            addr.nTime = GetTime() - GetRand(nOneWeek) - nOneWeek;
+            vFixedSeeds.push_back(addr);
+        }
+    }
+
+    virtual const CBlock& GenesisBlock() const { return genesis; }
+    virtual Network NetworkID() const { return CChainParams::MAIN; }
+
+    virtual const vector<CAddress>& FixedSeeds() const {
+        return vFixedSeeds;
+    }
+protected:
+    CBlock genesis;
+    vector<CAddress> vFixedSeeds;
+};
+static CMainParams mainParams;
+
+
+//
+// Testnet (v3)
+//
+class CTestNetParams : public CMainParams {
+public:
+    CTestNetParams() {
+        // The message start string is designed to be unlikely to occur in normal data.
+        // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+        // a large 4-byte int at any alignment.
+        pchMessageStart[0] = 0xda;
+        pchMessageStart[1] = 0xaf;
+        pchMessageStart[2] = 0xa5;
+        pchMessageStart[3] = 0xba;
+        //how to do
+        vAlertPubKey = ParseHex("04302390343f91cc401d56d68b123028bf52e5fca1939df127f63c6467cdf9c8e2c14b61104cf817d0b780da337893ecc4aaff1309e536162dabbdb45200ca2b0a");
+        nDefaultPort = 19336;
+        nRPCPort = 19337;
+        strDataDir = "testnet3";
+
+        // Modify the testnet genesis block so the timestamp is valid for a later start.
+        genesis.nTime    = 1396255061;
+        genesis.nNonce   = 3250989159;
+        hashGenesisBlock = genesis.GetHash();
+        assert(hashGenesisBlock == uint256("0x8e8b634d2f2800398261b7adcfbb6ace490e1746e62123ec2bf8010f9fc98b17"));
+
+        vFixedSeeds.clear();
+        vSeeds.clear();
+        //how to do feathercoin
+        vSeeds.push_back(CDNSSeedData("bitcoin.petertodd.org", "testnet-seed.bitcoin.petertodd.org"));
+        vSeeds.push_back(CDNSSeedData("bluematt.me", "testnet-seed.bluematt.me"));
+
+        // Boost sucks, and should not be used. Workaround for Boost not being compatible with C++11;
+        
+        base58Prefixes[PUBKEY_ADDRESS] = list_of(111);
+        base58Prefixes[SCRIPT_ADDRESS] = list_of(196);
+        base58Prefixes[SECRET_KEY]     = list_of(239);
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x35)(0x87)(0xCF);
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x35)(0x83)(0x94);
+    }
+    virtual Network NetworkID() const { return CChainParams::TESTNET; }
+};
+static CTestNetParams testNetParams;
+
+
+//
+// Regression test
+//
+class CRegTestParams : public CTestNetParams {
+public:
+    CRegTestParams() {
+        pchMessageStart[0] = 0xfa;
+        pchMessageStart[1] = 0xbf;
+        pchMessageStart[2] = 0xb5;
+        pchMessageStart[3] = 0xda;
+        nSubsidyHalvingInterval = 150;
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 1);
+        genesis.nTime = 1296688602;
+        genesis.nBits = 0x207fffff;
+        genesis.nNonce = 2;
+        hashGenesisBlock = genesis.GetHash();
+        nDefaultPort = 18444;
+        strDataDir = "regtest";
+        assert(hashGenesisBlock == uint256("0x8e8b634d2f2800398261b7adcfbb6ace490e1746e62123ec2bf8010f9fc98b17"));
+
+        vSeeds.clear();  // Regtest mode doesn't have any DNS seeds.
+    }
+
+    virtual bool RequireRPCPassword() const { return false; }
+    virtual Network NetworkID() const { return CChainParams::REGTEST; }
+};
+//static CRegTestParams regTestParams;
+
+static CChainParams *pCurrentParams = &mainParams;
+
+const CChainParams &Params() {
+    return *pCurrentParams;
+}
+
+void SelectParams(CChainParams::Network network) {
+    switch (network) {
+        case CChainParams::MAIN:
+            pCurrentParams = &mainParams;
+            break;
+        case CChainParams::TESTNET:
+            pCurrentParams = &testNetParams;
+            break;
+        case CChainParams::REGTEST:
+            //pCurrentParams = &regTestParams;
+            pCurrentParams = &testNetParams;
+            break;
+        default:
+            assert(false && "Unimplemented network");
+            return;
+    }
+}
+
+bool SelectParamsFromCommandLine() {
+    bool fRegTest = GetBoolArg("-regtest", false);
+    bool fTestNet = GetBoolArg("-testnet", false);
+
+    if (fTestNet && fRegTest) {
+        return false;
+    }
+
+    if (fRegTest) {
+        SelectParams(CChainParams::REGTEST);
+    } else if (fTestNet) {
+        SelectParams(CChainParams::TESTNET);
+    } else {
+        SelectParams(CChainParams::MAIN);
+    }
+    return true;
+}
---
+++ src/qt/moc_bitcoingui.cpp
@@ -0,0 +1,255 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'bitcoingui.h'
+**
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "bitcoingui.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'bitcoingui.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.3.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_BitcoinGUI_t {
+    QByteArrayData data[41];
+    char stringdata[525];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_BitcoinGUI_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_BitcoinGUI_t qt_meta_stringdata_BitcoinGUI = {
+    {
+QT_MOC_LITERAL(0, 0, 10),
+QT_MOC_LITERAL(1, 11, 11),
+QT_MOC_LITERAL(2, 23, 0),
+QT_MOC_LITERAL(3, 24, 3),
+QT_MOC_LITERAL(4, 28, 17),
+QT_MOC_LITERAL(5, 46, 5),
+QT_MOC_LITERAL(6, 52, 12),
+QT_MOC_LITERAL(7, 65, 7),
+QT_MOC_LITERAL(8, 73, 5),
+QT_MOC_LITERAL(9, 79, 5),
+QT_MOC_LITERAL(10, 85, 5),
+QT_MOC_LITERAL(11, 91, 3),
+QT_MOC_LITERAL(12, 95, 19),
+QT_MOC_LITERAL(13, 115, 6),
+QT_MOC_LITERAL(14, 122, 20),
+QT_MOC_LITERAL(15, 143, 18),
+QT_MOC_LITERAL(16, 162, 9),
+QT_MOC_LITERAL(17, 172, 19),
+QT_MOC_LITERAL(18, 192, 4),
+QT_MOC_LITERAL(19, 197, 4),
+QT_MOC_LITERAL(20, 202, 6),
+QT_MOC_LITERAL(21, 209, 4),
+QT_MOC_LITERAL(22, 214, 7),
+QT_MOC_LITERAL(23, 222, 16),
+QT_MOC_LITERAL(24, 239, 15),
+QT_MOC_LITERAL(25, 255, 20),
+QT_MOC_LITERAL(26, 276, 17),
+QT_MOC_LITERAL(27, 294, 4),
+QT_MOC_LITERAL(28, 299, 18),
+QT_MOC_LITERAL(29, 318, 20),
+QT_MOC_LITERAL(30, 339, 11),
+QT_MOC_LITERAL(31, 351, 14),
+QT_MOC_LITERAL(32, 366, 12),
+QT_MOC_LITERAL(33, 379, 22),
+QT_MOC_LITERAL(34, 402, 17),
+QT_MOC_LITERAL(35, 420, 33),
+QT_MOC_LITERAL(36, 454, 6),
+QT_MOC_LITERAL(37, 461, 21),
+QT_MOC_LITERAL(38, 483, 13),
+QT_MOC_LITERAL(39, 497, 12),
+QT_MOC_LITERAL(40, 510, 14)
+    },
+    "BitcoinGUI\0receivedURI\0\0uri\0"
+    "setNumConnections\0count\0setNumBlocks\0"
+    "message\0title\0style\0bool*\0ret\0"
+    "setEncryptionStatus\0status\0"
+    "handlePaymentRequest\0SendCoinsRecipient\0"
+    "recipient\0incomingTransaction\0date\0"
+    "unit\0amount\0type\0address\0gotoOverviewPage\0"
+    "gotoHistoryPage\0gotoReceiveCoinsPage\0"
+    "gotoSendCoinsPage\0addr\0gotoSignMessageTab\0"
+    "gotoVerifyMessageTab\0openClicked\0"
+    "optionsClicked\0aboutClicked\0"
+    "showHelpMessageClicked\0trayIconActivated\0"
+    "QSystemTrayIcon::ActivationReason\0"
+    "reason\0showNormalIfMinimized\0fToggleHidden\0"
+    "toggleHidden\0detectShutdown"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_BitcoinGUI[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      26,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,  144,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       4,    1,  147,    2, 0x0a /* Public */,
+       6,    1,  150,    2, 0x0a /* Public */,
+       7,    4,  153,    2, 0x0a /* Public */,
+       7,    3,  162,    2, 0x2a /* Public | MethodCloned */,
+      12,    1,  169,    2, 0x0a /* Public */,
+      14,    1,  172,    2, 0x0a /* Public */,
+      17,    5,  175,    2, 0x0a /* Public */,
+      23,    0,  186,    2, 0x08 /* Private */,
+      24,    0,  187,    2, 0x08 /* Private */,
+      25,    0,  188,    2, 0x08 /* Private */,
+      26,    1,  189,    2, 0x08 /* Private */,
+      26,    0,  192,    2, 0x28 /* Private | MethodCloned */,
+      28,    1,  193,    2, 0x08 /* Private */,
+      28,    0,  196,    2, 0x28 /* Private | MethodCloned */,
+      29,    1,  197,    2, 0x08 /* Private */,
+      29,    0,  200,    2, 0x28 /* Private | MethodCloned */,
+      30,    0,  201,    2, 0x08 /* Private */,
+      31,    0,  202,    2, 0x08 /* Private */,
+      32,    0,  203,    2, 0x08 /* Private */,
+      33,    0,  204,    2, 0x08 /* Private */,
+      34,    1,  205,    2, 0x08 /* Private */,
+      37,    1,  208,    2, 0x08 /* Private */,
+      37,    0,  211,    2, 0x28 /* Private | MethodCloned */,
+      39,    0,  212,    2, 0x08 /* Private */,
+      40,    0,  213,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString,    3,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString, QMetaType::UInt, 0x80000000 | 10,    8,    7,    9,   11,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString, QMetaType::UInt,    8,    7,    9,
+    QMetaType::Void, QMetaType::Int,   13,
+    QMetaType::Bool, 0x80000000 | 15,   16,
+    QMetaType::Void, QMetaType::QString, QMetaType::Int, QMetaType::LongLong, QMetaType::QString, QMetaType::QString,   18,   19,   20,   21,   22,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   27,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   27,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   27,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 35,   36,
+    QMetaType::Void, QMetaType::Bool,   38,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void BitcoinGUI::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        BitcoinGUI *_t = static_cast<BitcoinGUI *>(_o);
+        switch (_id) {
+        case 0: _t->receivedURI((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 1: _t->setNumConnections((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 2: _t->setNumBlocks((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->message((*reinterpret_cast< const QString(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2])),(*reinterpret_cast< uint(*)>(_a[3])),(*reinterpret_cast< bool*(*)>(_a[4]))); break;
+        case 4: _t->message((*reinterpret_cast< const QString(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2])),(*reinterpret_cast< uint(*)>(_a[3]))); break;
+        case 5: _t->setEncryptionStatus((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 6: { bool _r = _t->handlePaymentRequest((*reinterpret_cast< const SendCoinsRecipient(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
+        case 7: _t->incomingTransaction((*reinterpret_cast< const QString(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< qint64(*)>(_a[3])),(*reinterpret_cast< const QString(*)>(_a[4])),(*reinterpret_cast< const QString(*)>(_a[5]))); break;
+        case 8: _t->gotoOverviewPage(); break;
+        case 9: _t->gotoHistoryPage(); break;
+        case 10: _t->gotoReceiveCoinsPage(); break;
+        case 11: _t->gotoSendCoinsPage((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 12: _t->gotoSendCoinsPage(); break;
+        case 13: _t->gotoSignMessageTab((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 14: _t->gotoSignMessageTab(); break;
+        case 15: _t->gotoVerifyMessageTab((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 16: _t->gotoVerifyMessageTab(); break;
+        case 17: _t->openClicked(); break;
+        case 18: _t->optionsClicked(); break;
+        case 19: _t->aboutClicked(); break;
+        case 20: _t->showHelpMessageClicked(); break;
+        case 21: _t->trayIconActivated((*reinterpret_cast< QSystemTrayIcon::ActivationReason(*)>(_a[1]))); break;
+        case 22: _t->showNormalIfMinimized((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 23: _t->showNormalIfMinimized(); break;
+        case 24: _t->toggleHidden(); break;
+        case 25: _t->detectShutdown(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (BitcoinGUI::*_t)(const QString & );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&BitcoinGUI::receivedURI)) {
+                *result = 0;
+            }
+        }
+    }
+}
+
+const QMetaObject BitcoinGUI::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_BitcoinGUI.data,
+      qt_meta_data_BitcoinGUI,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *BitcoinGUI::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *BitcoinGUI::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_BitcoinGUI.stringdata))
+        return static_cast<void*>(const_cast< BitcoinGUI*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int BitcoinGUI::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 26)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 26;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 26)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 26;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void BitcoinGUI::receivedURI(const QString & _t1)
+{
+    void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+QT_END_MOC_NAMESPACE
